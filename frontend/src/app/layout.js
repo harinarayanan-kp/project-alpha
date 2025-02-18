d45c0abaada13adf556c1a2c52bd79bb5d1de658
Harinarayanan K P <harikpaug18@gmail.com>
2025-02-18 22:14:26 +0530

    Initial commit

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,17 @@
+
+import "./globals.css";
+
+export const metadata = {
+  title: "Events GEC",
+  description: "Event listing platform for GECT",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
