d9848eeaaf905aa60d1714c19b63647972f54c0a
Shreya Jayaraj <shreya291104@gmail.com>
2025-02-19 01:36:07 +0530

    deployed db

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,21 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 4000
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
 
 mongoose.connect('mongodb+srv://teamdaash:123daash123@backenddb.2grsm.mongodb.net/')
 .then(() => {
   console.log("DB connected")
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
 })
+.catch(() => {
+  console.log("DB connection failed")
+}) 
