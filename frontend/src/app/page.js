7bc4b31b8fa43e29dead3de0b7490b21bf37a3e5
devikarajeevan <devikarajeevan2004@gmail.com>
2025-02-19 23:07:49 +0530

    navbar footer admin dashboard

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,14 +1,15 @@
 // import Image from "next/image";
 
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       Events list<br/>
-      footer
-      testing
+      
+      <Footer/>
     </div>
   );
 }
