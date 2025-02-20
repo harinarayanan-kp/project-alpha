421f6eb15046c196cd926cdeb6ee6924004a4991
Shreya Jayaraj <shreya291104@gmail.com>
2025-02-21 00:39:44 +0530

    get club by id done

--- backend/controllers/club.controller.js
+++ backend/controllers/club.controller.js
@@ -17,7 +17,20 @@ const getClubs = async(req,res) => {
     try{
         const clubs =await Club.find({})
 
-        res.status(201).json({clubs})
+        res.status(200).json({clubs})
+    }catch(err){
+        console.log(err)
+        res.status(500).json({ message: err.message})
+    }
+}
+
+const getClubbyId = async(req,res) => {
+    try{
+        const club =await Club.findById(req.params.id);
+        if (!club) {
+            return res.status(404).json({ message: "Club not found" });
+          }
+        res.status(201).json({club})
     }catch(err){
         console.log(err)
         res.status(500).json({ message: err.message})
@@ -27,4 +40,5 @@ const getClubs = async(req,res) => {
 module.exports = {
     createClub,
     getClubs,
+    getClubbyId
 }
--- backend/routes/club.route.js
+++ backend/routes/club.route.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
-const {createClub, getClubs} = require("../controllers/club.controller.js")
+const {createClub, getClubs, getClubbyId} = require("../controllers/club.controller.js")
 
 router.post("/create-club",createClub)
-router.post("/get-clubs",getClubs)
+router.get("/get-clubs",getClubs)
+router.get("/get-club/:id", getClubbyId);
 
 module.exports = router;
